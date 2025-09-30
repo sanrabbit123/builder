06c14ef75945103338594f75bdfb5bd9aabe737b
uragen <uragenbooks@gmail.com>
2025-09-30 16:51:33 +0900

    turtle_250930_v4

--- script/builder.ts
+++ script/builder.ts
@@ -2,12 +2,8 @@ import path from "path";
 import { existsSync } from "fs";
 import fsPromise from "fs/promises";
 import process from "process";
-import { Mother } from "../source/apps/mother.ts";
-import { RouterFunction, RouterObject, RouterUnit } from "../source/apps/classStorage/dictionary.ts";
 
 
-
-console.log(Mother);
 
 
 
